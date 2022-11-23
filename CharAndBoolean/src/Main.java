6b95a43a84e78adeb14847a61b117bb47fd5bccf
cptntz2119 <zhangdong2119@hotmail.com>
2022-11-22 17:12:00 -0800

    char and boolean

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyright = '\u00A9';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyright);
+
+        boolean myTureBooleanVal = true;
+        boolean myFalseBooleanVal = false;
+        boolean isCustomerOVerTwentyOne = true;
+
+    }
+}
