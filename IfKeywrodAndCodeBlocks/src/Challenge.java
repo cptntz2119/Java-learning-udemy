302602f3cffa96b71f74b7f6885a81ce316f04ed
cptntz2119 <zhangdong2119@hotmail.com>
2022-11-24 19:47:41 -0800

    method introduced

--- IfKeywrodAndCodeBlocks/src/Challenge.java
+++ IfKeywrodAndCodeBlocks/src/Challenge.java
@@ -1,39 +1,19 @@
 public class Challenge {
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        int finalScore = score;
-         if (gameOver) {
-             finalScore += (levelCompleted * bonus);
-             System.out.println("your final score was " + finalScore);
-         }
-
-         score = 10000;
-         levelCompleted = 8;
-         bonus = 200;
-
-         finalScore = score;
-
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("your final score was " + finalScore);
-        }
-
-        calculateScore(true, 800, 5, 100);
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("the high score " + highScore);
+        calculateScore(true, 10000, 8, 200);
     }
 
-    public static void calculateScore (boolean gameOver, int score, int levelCompleted, int bonus) {
-
+    public static int calculateScore (boolean gameOver, int score, int levelCompleted, int bonus) {
 
         int finalScore = score;
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("your final score was from a method " + finalScore);
+//            System.out.println("your final score was from a method " + finalScore);
         }
+        return finalScore;
     }
 
 
