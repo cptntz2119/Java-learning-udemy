a99463b999794e1650a7745e7b9a540aec4caa90
cptntz2119 <zhangdong2119@hotmail.com>
2022-11-28 20:47:03 -0800

    coding exercise 2-8

--- CodingExercise2To8/src/Main.java
+++ CodingExercise2To8/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         printConversion(1.25);
         printConversion(95.75);
+        System.out.println(shouldWakeUp(false, 2));
     }
 
     public static long toMilesPerHour(double kilometersPerHour) {
@@ -12,5 +13,15 @@ public class Main {
     public static void printConversion (double kilometersPerHour) {
         System.out.println( (kilometersPerHour >= 0 ) ? kilometersPerHour + " km/h = "
                 + toMilesPerHour(kilometersPerHour) + " mi/h" : "Invalid Value");
+    }
+
+    public static boolean shouldWakeUp (boolean isCurrentlyBarking, int hourOfDay){
+        if ((isCurrentlyBarking ) &&
+                ((hourOfDay >= 0 && hourOfDay < 8) || (hourOfDay > 22 && hourOfDay < 24))){
+                return true;
+            }
+
+        return false;
+
     }
 }
