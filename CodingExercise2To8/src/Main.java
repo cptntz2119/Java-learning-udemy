804f8111757c81311e04fb6dab74be217113e95a
cptntz2119 <zhangdong2119@hotmail.com>
2022-11-29 20:03:28 -0800

    coding exercise 2-8

--- CodingExercise2To8/src/Main.java
+++ CodingExercise2To8/src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         printConversion(1.25);
         printConversion(95.75);
         System.out.println(shouldWakeUp(false, 2));
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-2500);
+        printMegaBytesAndKiloBytes(5000);
+        System.out.println(areEqualByThreeDecimalPlaces(-3.175,-3.1756));
     }
 
     public static long toMilesPerHour(double kilometersPerHour) {
@@ -33,5 +37,39 @@ public class Main {
         }
         return false;
     }
+
+    public static void printMegaBytesAndKiloBytes (int kiloBytes) {
+        if (kiloBytes >= 0) {
+            System.out.println(kiloBytes + " KB= " + kiloBytes / 1024 + " MB " + kiloBytes % 1024 + " KB");
+        }else {
+            System.out.println("Invalid Value");
+        }
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces (double num1, double num2) {
+        if ((int)(num1*1000) == (int)(num2*1000)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean hasEqualSum (int num1, int num2, int num3) {
+        if (num1 == num3 - num2) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean hasTeen (int num1, int num2, int num3) {
+        return isTeen(num1) || isTeen(num2) || isTeen(num3);
+    }
+    public static boolean isTeen (int nr1){
+            return (nr1 >= 13 && nr1 <= 19);
+    }
+
+
+
+
 
 }
