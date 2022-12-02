b233096c963428b94ce854cdde1222e7da88c194
cptntz2119 <zhangdong2119@hotmail.com>
2022-12-01 18:17:06 -0800

    switch statement challenge

---
+++ switchChallenge/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        String word = "A";
+        switch (word) {
+            case "A":
+                System.out.println(word + " in NATO means Able");
+                break;
+            case "B":
+                System.out.println(word + " in NATO means Baler");
+                break;
+            case "C":
+                System.out.println(word + " in NATO means Charlie");
+                break;
+            case "D":
+                System.out.println(word + " in NATO means Dog");
+                break;
+            default:
+                System.out.println("invalid value");
+        }
+
+
+    }}
