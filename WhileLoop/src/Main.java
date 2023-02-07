7314704e5358a539a01aed5fd1f7da1cc57d232d
cptntz2119 <zhangdong2119@hotmail.com>
2023-02-07 12:46:37 -0800

    while loop and continue;

---
+++ WhileLoop/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+
+        int j = 1;
+        while (j <= 5){
+            System.out.println(j);
+            j++;
+        }
+
+        int k = 1;
+        boolean isReady = false;
+        do {
+            if (k > 5){
+                break;
+            }
+
+            System.out.println(k);
+            k++;
+            isReady = (j > 0);
+        }while (isReady);
+
+        int l = 0;
+        while (l < 50){
+            l += 5;
+            if (l % 25 == 0){
+                continue;
+            }
+            System.out.println(l + " ");
+        }
+    }
+}
